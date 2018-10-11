36c1abd74efafb59138c205c14ae667243f4801a
Roger Tello Araez <rogerteello97@gmail.com>
2018-10-11 12:37:18 +0200

    modified lib name

--- TextureImporter.h
+++ TextureImporter.h
@@ -10,9 +10,9 @@
 #include "DevIL Windows SDK\include\IL\ilu.h"
 #include "DevIL Windows SDK\include\IL\ilut.h"
 
-#pragma comment(lib, "DevIL Windows SDK/lib/x86/Release/DevIL.lib")
-#pragma comment(lib, "DevIL Windows SDK/lib/x86/Release/ILU.lib")
-#pragma comment(lib, "DevIL Windows SDK/lib/x86/Release/ILUT.lib")
+#pragma comment(lib, "DevIL Windows SDK/libx86/x86/Release/DevIL.lib")
+#pragma comment(lib, "DevIL Windows SDK/libx86/x86/Release/ILU.lib")
+#pragma comment(lib, "DevIL Windows SDK/libx86/x86/Release/ILUT.lib")
 
 
 class TextureImporter : public Importer
