8cfc07fec6aee16886914fd8243f1edc3db97dcb
Roger Tello Araez <rogerteello97@gmail.com>
2018-10-11 15:54:14 +0200

    Revert "try to fix my live"
    This reverts commit dce540e160e3b34f2f4bd95751df77d5a89bd17a.

---
+++ UI_ConfigurationPanel.cpp
@@ -0,0 +1,38 @@
+#include "UI_ConfigurationPanel.h"
+#include "imgui_dock.h"
+#include "Application.h"
+
+UI_ConfigurationPanel::UI_ConfigurationPanel()
+{
+	type = Panel_Types::CONFIGURATION_PANEL;
+}
+
+UI_ConfigurationPanel::~UI_ConfigurationPanel()
+{
+}
+
+bool UI_ConfigurationPanel::Start()
+{
+	show = true; 
+
+	return true;
+}
+
+bool UI_ConfigurationPanel::Update()
+{
+	if (show == false)
+		return false; 
+
+	if (ImGui::Begin("Configuration", &show ,NULL))
+	{
+		App->DisplayConfigData();
+
+		for (int i = 0; App->GetModuleAt(i) != nullptr; i++)
+		{
+			App->GetModuleAt(i)->PrintConfigData();
+		}
+	}
+	ImGui::End();  
+
+	return true;
+}
