c85b7fe5595386d7bad205289e26c5f6fe66134f
polferrando98 <pol.ferrando.98@gmail.com>
2018-11-17 16:30:14 +0100

    Meta meshes now hidden

--- UI_ExplorerPanel.cpp
+++ UI_ExplorerPanel.cpp
@@ -9,7 +9,7 @@
 
 UI_ExplorerPanel::UI_ExplorerPanel()
 {
-	show_item_options = false; 
+	show_item_options = false;
 	name = "Explorer";
 
 	folder_mat = nullptr;
@@ -27,7 +27,7 @@ UI_ExplorerPanel::~UI_ExplorerPanel()
 
 void UI_ExplorerPanel::DrawExplorerRecursive(std::string folder)
 {
-	string item_name; 
+	string item_name;
 
 	if (App->file_system->IsFolder(folder.c_str()))
 	{
@@ -38,7 +38,7 @@ void UI_ExplorerPanel::DrawExplorerRecursive(std::string folder)
 			ImGui::Image((ImTextureID)folder_mat->GetDiffuseTexture()->GetTextureID(), ImVec2(18, 15), ImVec2(0, 1), ImVec2(1, 0));
 			ImGui::SameLine();
 		}
-			
+
 		if (ImGui::TreeNodeEx(item_name.c_str()))
 		{
 			//If it has childs we call them recursively
@@ -52,8 +52,8 @@ void UI_ExplorerPanel::DrawExplorerRecursive(std::string folder)
 					string next_file = folder + '\\' + (*it);
 					DrawExplorerRecursive(next_file);
 				}
-					
-			}				
+
+			}
 			ImGui::TreePop();
 		}
 	}
@@ -62,92 +62,100 @@ void UI_ExplorerPanel::DrawExplorerRecursive(std::string folder)
 		item_name = App->file_system->GetLastPathItem(folder.c_str(), true);
 		file_extension ext = App->file_system->GetFileExtension(folder.c_str());
 
+		bool must_hide = false;
+
 		switch (App->file_system->GetFileType(folder.c_str()))
 		{
-			case file_type::FT_3DMODEL:
+		case file_type::FT_3DMODEL:
 
-				if (mesh_mat == nullptr)
-					break; 
-
-				ImGui::Image((ImTextureID)mesh_mat->GetDiffuseTexture()->GetTextureID(), ImVec2(18, 18), ImVec2(0, 1), ImVec2(1, 0));
-				ImGui::SameLine();
-				break; 
-
-			case file_type::FT_IMAGE:
-
-				/*if (App->file_system->GetFileExtension(folder.c_str()) == FX_DDS)
-				{
-					if (DDS_mat == nullptr)
-						break;
-
-					ImGui::Image((ImTextureID)DDS_mat->GetDiffuseTexture()->GetTextureID(), ImVec2(18, 18));
-					ImGui::SameLine();
-				}
-				else if (App->file_system->GetFileExtension(folder.c_str()) == FX_TGA)
-				{
-					if (TGA_mat == nullptr)
-						break;
-
-					ImGui::Image((ImTextureID)TGA_mat->GetDiffuseTexture()->GetTextureID(), ImVec2(18, 18));
-					ImGui::SameLine();
-				}
-*/
-				//else
-				//{
-					if (image_mat == nullptr)
-						break;
-
-					ImGui::Image((ImTextureID)image_mat->GetDiffuseTexture()->GetTextureID(), ImVec2(18, 18), ImVec2(0, 1), ImVec2(1, 0));
-					ImGui::SameLine();
-				//}
-	
+			if (mesh_mat == nullptr)
 				break;
 
-			case file_type::FT_FONT:
+			ImGui::Image((ImTextureID)mesh_mat->GetDiffuseTexture()->GetTextureID(), ImVec2(18, 18), ImVec2(0, 1), ImVec2(1, 0));
+			ImGui::SameLine();
+			break;
 
-				if (font_mat == nullptr)
-					break;
+		case file_type::FT_IMAGE:
 
-				ImGui::Image((ImTextureID)font_mat->GetDiffuseTexture()->GetTextureID(), ImVec2(18, 18), ImVec2(0, 1), ImVec2(1, 0));
-				ImGui::SameLine();
+			/*if (App->file_system->GetFileExtension(folder.c_str()) == FX_DDS)
+			{
+			if (DDS_mat == nullptr)
+			break;
+
+			ImGui::Image((ImTextureID)DDS_mat->GetDiffuseTexture()->GetTextureID(), ImVec2(18, 18));
+			ImGui::SameLine();
+			}
+			else if (App->file_system->GetFileExtension(folder.c_str()) == FX_TGA)
+			{
+			if (TGA_mat == nullptr)
+			break;
+
+			ImGui::Image((ImTextureID)TGA_mat->GetDiffuseTexture()->GetTextureID(), ImVec2(18, 18));
+			ImGui::SameLine();
+			}
+			*/
+			//else
+			//{
+			if (image_mat == nullptr)
 				break;
 
-			case file_type::FT_SCENE:
+			ImGui::Image((ImTextureID)image_mat->GetDiffuseTexture()->GetTextureID(), ImVec2(18, 18), ImVec2(0, 1), ImVec2(1, 0));
+			ImGui::SameLine();
+			//}
 
-				if (scene_mat == nullptr)
-					break;
+			break;
 
-				ImGui::Image((ImTextureID)scene_mat->GetDiffuseTexture()->GetTextureID(), ImVec2(18, 18), ImVec2(0, 1), ImVec2(1, 0));
-				ImGui::SameLine();
+		case file_type::FT_FONT:
+
+			if (font_mat == nullptr)
 				break;
 
-			case file_type::FT_PREFAB:
+			ImGui::Image((ImTextureID)font_mat->GetDiffuseTexture()->GetTextureID(), ImVec2(18, 18), ImVec2(0, 1), ImVec2(1, 0));
+			ImGui::SameLine();
+			break;
 
-				if (obj_mat == nullptr)
-					break;
+		case file_type::FT_SCENE:
 
-				ImGui::Image((ImTextureID)obj_mat->GetDiffuseTexture()->GetTextureID(), ImVec2(18, 18), ImVec2(0, 1), ImVec2(1, 0));
-				ImGui::SameLine();
+			if (scene_mat == nullptr)
 				break;
-				
-			default: 
+
+			ImGui::Image((ImTextureID)scene_mat->GetDiffuseTexture()->GetTextureID(), ImVec2(18, 18), ImVec2(0, 1), ImVec2(1, 0));
+			ImGui::SameLine();
+			break;
+
+		case file_type::FT_PREFAB:
+
+			if (obj_mat == nullptr)
+				break;
+
+			ImGui::Image((ImTextureID)obj_mat->GetDiffuseTexture()->GetTextureID(), ImVec2(18, 18), ImVec2(0, 1), ImVec2(1, 0));
+			ImGui::SameLine();
+			break;
+
+		default:
+			if (item_name == "MetaMeshes")
+				must_hide = true;
+			else {
 				ImGui::Image((ImTextureID)App->resources->material_importer->GetCheckerTexture()->GetTextureID(), ImVec2(18, 18), ImVec2(0, 1), ImVec2(1, 0));
 				ImGui::SameLine();
-			
-		}
-			
-		ImGui::MenuItem(item_name.c_str());
+			}
 
-		if (ImGui::IsItemClicked(1))
-		{
-			show_item_options = true;
-			ImGui::OpenPopup("ExplorerOptions");
-			item_selected = item_name; 
-		}		
+		}
+
+		if (!must_hide) {
+			ImGui::MenuItem(item_name.c_str());
+
+			if (ImGui::IsItemClicked(1))
+			{
+				show_item_options = true;
+				ImGui::OpenPopup("ExplorerOptions");
+				item_selected = item_name;
+			}
+		}
 	}
 
 	if (show_item_options && item_name == item_selected)
-	{	
+	{
 		if (ImGui::BeginPopup("ExplorerOptions"))
 		{
 			if (ImGui::MenuItem("Load Item"))
@@ -161,75 +169,75 @@ void UI_ExplorerPanel::DrawExplorerRecursive(std::string folder)
 
 					if (App->file_system->IsFileInDirectory(directory, meta_name.c_str()))
 					{
-						Prefab* curr_prf = (Prefab*)App->resources->Get(RES_PREFAB, App->file_system->GetLastPathItem(folder).c_str()); 
+						Prefab* curr_prf = (Prefab*)App->resources->Get(RES_PREFAB, App->file_system->GetLastPathItem(folder).c_str());
 						curr_prf->LoadFromBinary();
-						curr_prf->GetRootGameObject()->ModifyIDSet(); 
+						curr_prf->GetRootGameObject()->ModifyIDSet();
 
 						App->scene->SetSelectedGameObject(curr_prf->GetRootGameObject());
-					
+
 					}
 					else
 					{
 						GameObject* parent = App->resources->mesh_importer->CreateFBXMesh(folder.c_str());
 						App->scene->SetSelectedGameObject(parent);
 					}
-					
+
 					App->camera->GetEditorCamera()->center_next_frame = true;
-					
+
 				}
-					
+
 				else if (App->file_system->GetFileExtension(folder.c_str()) == FX_PNG || App->file_system->GetFileExtension(folder.c_str()) == FX_DDS || App->file_system->GetFileExtension(folder.c_str()) == FX_JPG)
 				{
-					string lib_item = App->file_system->GetLastPathItem(string(folder.c_str() + string(".dds")).c_str()); 
+					string lib_item = App->file_system->GetLastPathItem(string(folder.c_str() + string(".dds")).c_str());
 					Material* new_mat = (Material*)App->resources->CreateNewResource(RES_MATERIAL); //LOOK
 					App->resources->material_importer->LoadFromBinary(lib_item.c_str(), new_mat);
 				}
-					
+
 				else if (App->file_system->GetFileType(folder.c_str()) == FT_SCENE)
 				{
 					App->scene->LoadScene(item_name.c_str());
-					CONSOLE_LOG("SCENE LOADED SUCCESFULLY"); 
+					CONSOLE_LOG("SCENE LOADED SUCCESFULLY");
 				}
-					
+
 
 				else if (App->file_system->GetFileType(folder.c_str()) == FT_PREFAB)
 				{
-					string obj_name = App->file_system->GetLastPathItem(folder.c_str()); 
+					string obj_name = App->file_system->GetLastPathItem(folder.c_str());
 
-					GameObject* new_go = App->scene->LoadPrefab(obj_name.c_str()); 
+					GameObject* new_go = App->scene->LoadPrefab(obj_name.c_str());
 
 				}
-					
-			
+
+
 			}
 
-			if (ImGui::MenuItem("Delete Resource")) 
+			if (ImGui::MenuItem("Delete Resource"))
 			{
-				
+
 			}
 
 			if (ImGui::MenuItem("Open In Explorer"))
 			{
-				const char* parent_folder = App->file_system->DeleteLastPathItem(folder.c_str()).c_str(); 
+				const char* parent_folder = App->file_system->DeleteLastPathItem(folder.c_str()).c_str();
 				ShellExecute(NULL, "open", parent_folder, NULL, NULL, SW_SHOWMINIMIZED);
 			}
-			
+
 			ImGui::EndPopup();
 		}
 	}
 
-	return; 
+	return;
 }
 
 bool UI_ExplorerPanel::Start()
 {
-	show = true; 
-	root_folder = App->file_system->GetAssetsPath(); 
-	item_selected = ""; 
+	show = true;
+	root_folder = App->file_system->GetAssetsPath();
+	item_selected = "";
 
 	folder_mat = (Material*)App->resources->Get(RES_MATERIAL, "FolderIcon");
-	folder_mat->LoadToMemory(); 
-	folder_mat->reference_counting++; 
+	folder_mat->LoadToMemory();
+	folder_mat->reference_counting++;
 
 	obj_mat = (Material*)App->resources->Get(RES_MATERIAL, "GameObjectIcon");
 	obj_mat->LoadToMemory();
